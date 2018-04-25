f4db027629928a4290b0fdf011d89231f51eb51e
Jeff Trimmer <jmt41860@ucmo.edu>
2018-04-25 12:39:47 -0700

    Initialize project. First commit

---
+++ src/main/java/org/eclairj/core/Eclair.java
@@ -0,0 +1,19 @@
+package org.eclairj.core;
+
+import feign.*;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+
+public interface Eclair {
+    Eclair api = Feign.builder()
+            .decoder(new JacksonDecoder())
+            .encoder(new JacksonEncoder())
+            .requestInterceptor(new BasicAuthRequestInterceptor("", "qazwsxedc"))
+            .target(Eclair.class, "http://54.202.231.72:8080");
+
+    @RequestLine("POST /")
+    @Headers("Content-Type: application/json")
+    @Body("{\"jsonrpc\": \"2.0\", \"method\": \"getinfo\"}")
+    NodeInfo getInfo();
+}
